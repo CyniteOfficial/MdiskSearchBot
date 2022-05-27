d281929801b4975fc5a1a3f9b73e2fefb0a07ff1
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-05-27 22:36:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,14 @@ User = Client(
 
 
 @Bot.on_message(filters.private & filters.command("start"))
-async def start_handler(_, event: Message):
-    await event.reply_photo("https://telegra.ph/file/f35d8b79281781574e6f4.jpg", caption=Config.START_MSG.format(event.from_user.mention),
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
-             InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")],
-            [InlineKeyboardButton("Help", callback_data="Help_msg"),
-             InlineKeyboardButton("About", callback_data="About_msg")])
+    async def start_handler(_, event: Message):
+        await event.reply_photo("https://telegra.ph/file/f35d8b79281781574e6f4.jpg",
+                                caption=Config.START_MSG.format(event.from_user.mention),
+                                reply_markup=InlineKeyboardMarkup([
+                                    [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+                                     InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")],
+                                    [InlineKeyboardButton("Help", callback_data="Help_msg"),
+                                     InlineKeyboardButton("About", callback_data="About_msg")]]))
         ])
     )
 

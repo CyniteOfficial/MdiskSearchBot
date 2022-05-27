ebe2bf4153bb55ef51fa4f6f88279ef3e9e071f5
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-05-27 12:19:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# (c) @AbirHasan2005
-# I just made this for searching a channel message from inline.
-# Maybe you can use this for something else.
-# I first made this for @AHListBot ...
-# Edit according to your use.
-# Edited to take all queries in incoming message, not in inline by @xendaddy
+# (c) @RoyalKrrishna
 
 from configs import Config
 from pyrogram import Client, filters, idle
 from pyrogram.errors import QueryIdInvalid
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, InlineQuery, InlineQueryResultArticle, \
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
     InputTextMessageContent
+from TeamTeleRoid.forcesub import ForceSub
 
 # Bot Client for Inline Search
 Bot = Client(
@@ -18,6 +14,7 @@ Bot = Client(
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
 )
+
 # User Client for Searching in Channel.
 User = Client(
     session_name=Config.USER_SESSION_STRING,
@@ -28,59 +25,145 @@ User = Client(
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start_handler(_, event: Message):
-    await event.reply_text(
-        "Hi, I am Messages Search Bot!\n\n"
-        "**Developer:** @AbirHasan2005\n"
-        "**Demo Bot:** @AHListBot",
+
+    await event.reply_text(Config.START_MSG.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Support Group", url="https://t.me/DevsZone"),
-             InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-            [InlineKeyboardButton("Developer - @AbirHasan2005")],
-            [InlineKeyboardButton("Search Inline", switch_inline_query_current_chat=""), InlineKeyboardButton("Go Inline", switch_inline_query="")]
+            [InlineKeyboardButton("Search Your Movie Here 🔍", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+             InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")],
+            [InlineKeyboardButton("Help", callback_data="Help_msg"),
+             InlineKeyboardButton("About", callback_data="About_msg")]
         ])
     )
 
+@Bot.on_message(filters.private & filters.command("help"))
+async def help_handler(_, event: Message):
 
-@Bot.on_message(filters.incoming)
-async def message_handler(_, event: Message):
-    answers = 'Results: \n\n'
-#     # If Search Query is Empty
-#     if event.text == "":
-#         await event.answer('')
-#         answers.append(
-#             InlineQueryResultArticle(
-#                 title="This is Inline Messages Search Bot!",
-#                 description="You can search Channel All Messages using this bot.",
-#                 input_message_content=InputTextMessageContent(
-#                     message_text="Using this Bot you can Search a Channel All Messages using this bot.\n\n"
-#                                  "Made by @AbirHasan2005",
-#                     disable_web_page_preview=True
-#                 ),
-#                 reply_markup=InlineKeyboardMarkup([
-#                     [InlineKeyboardButton("Search Here", switch_inline_query_current_chat="")],
-#                     [InlineKeyboardButton("Support Group", url="https://t.me/DevsZone"),
-#                      InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-#                     [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")]
-#                 ])
-#             )
-#         )
-#     # Search Channel Message using Search Query Words
-#     else:
-    async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
-        if message.text:
-            answers += "- `{}` \n".format(message.text.split("\n", 1)[0]))
-#                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Search Again", switch_inline_query_current_chat="")]]),
-#                 input_message_content=InputTextMessageContent(
-#                     message_text=message.text.markdown,
-#                     parse_mode="markdown",
-#                     disable_web_page_preview=True
-#                 )
-#             ))
+    await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Search Your Movie Here 🔍", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+             InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup"), 
+             InlineKeyboardButton("About", callback_data="About_msg")]
+        ])
+    )
+
+@Bot.on_inline_query()
+async def inline_handlers(_, event: InlineQuery):
+    answers = list()
+    # If Search Query is Empty
+
+    if event.query == "":
+        answers.append(
+            InlineQueryResultArticle(
+                title="This Is Mdisk Movie Search bot.🔍",
+                description="You Can Search All Movies Available On Mdisk.",
+                thumb_url="https://telegra.ph/file/2625b34c453088a8d687f.jpg", 
+                input_message_content=InputTextMessageContent(
+                    message_text="<b>Search Movie Name In Inline Mode Only.🔍\n\nOwner - @RoyalKrrishna</b></a>",
+
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("Search Your Movies Here 🔍", switch_inline_query_current_chat="")],
+                    [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+                     InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")],
+                    [InlineKeyboardButton("Report", url="https://t.me/RoyalKrrishna")]
+                ])
+            )
+        )
+    # Search Channel Message using Search Query Words
+    else:
+        async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.query):
+            if message.text:
+                thumb = None
+                f_text = message.text
+                msg_text = message.text.html
+                if "|||" in message.text:
+                    thumb = message.text.split("|||",1)[1].strip()
+                    f_text = message.text.split("|||",1)[0]
+                    msg_text = message.text.html.split("|||",1)[0]
+                answers.append(InlineQueryResultArticle(
+                    title="{}".format(f_text.split("\n", 1)[0]),
+                    description="{}".format(f_text.split("\n", 2)[-1]),
+                    thumb_url=thumb,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Search Again 🔍", switch_inline_query_current_chat=""), InlineKeyboardButton("Report❗", url="https://t.me/RoyalKrrishna")]]),
+                    input_message_content=InputTextMessageContent(
+                        message_text=msg_text,
+                        parse_mode="html",
+                        disable_web_page_preview=True
+                    )
+                ))
     try:
-        await event.reply_text(answers)
+        await event.answer(
+            results=answers,
+            cache_time=0
+        )
         print(f"[{Config.BOT_SESSION_NAME}] - Answered Successfully - {event.from_user.first_name}")
-    except:
+    except QueryIdInvalid:
         print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
+
+
+@Bot.on_callback_query()
+async def button(bot, cmd: CallbackQuery):
+        cb_data = cmd.data
+        if "About_msg" in cb_data:
+            await cmd.message.edit(
+			text=Config.ABOUT_BOT_TEXT,
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+						InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")
+					],
+					[
+						InlineKeyboardButton("Developer", url="https://t.me/RoyalKrrishna"),
+						InlineKeyboardButton("Home", callback_data="gohome")
+					]
+				]
+			),
+			parse_mode="html"
+		)
+        elif "Help_msg" in cb_data:
+            await cmd.message.edit(
+			text=Config.ABOUT_HELP_TEXT,
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("About", callback_data="About_msg"),
+						InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies")
+					], 
+                                        [
+						InlineKeyboardButton("Owner", url="https://t.me/RoyalKrrishna"),
+						InlineKeyboardButton("Home", callback_data="gohome")
+					]
+				]
+			),
+			parse_mode="html"
+		)
+        elif "gohome" in cb_data:
+            await cmd.message.edit(
+			text=Config.START_MSG.format(cmd.from_user.mention),
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("Search Your Movie Here 🔍", switch_inline_query_current_chat="")
+					],
+                                        [
+						InlineKeyboardButton("Help", callback_data="Help_msg"),
+						InlineKeyboardButton("About", callback_data="About_msg")
+					],
+					[
+						InlineKeyboardButton("Support", url="https://t.me/RoyalKrrishna"),
+						InlineKeyboardButton("Channel", url="https://t.me/iP_Movies")
+					]
+				]
+			),
+			parse_mode="html"
+		)
 
 # Start Clients
 Bot.start()

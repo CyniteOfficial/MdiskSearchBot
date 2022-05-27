4cd6d8168d68a10f6d7c1e6869aae07d32e222c6
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-05-27 21:28:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PredatorHackerzZ/MessageSearchBot.git /MessageSearchBot
-cd /MessageSearchBot
+git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
+cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py

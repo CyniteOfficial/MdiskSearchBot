fcec3d3c9f56ba2998f3c7863cd08894fe50622e
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2022-11-12 09:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def inline_handlers(_, event: Message):
             answers += f'**🍿 Title ➠ ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n📜 About ➠ ' + '' + f_text.split("\n", 2)[-1] + ' \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\nLink Will Auto Delete In 60Sec...⏰\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
     try:
         msg = await event.reply_text(answers)
-        await asyncio.sleep(60)
+        await asyncio.sleep(300)
         await event.delete()
         await msg.delete()
     except:

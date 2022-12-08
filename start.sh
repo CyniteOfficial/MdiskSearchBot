99bb8330f1131cf5763b95568b30e4f45741ff63
Etchial <98379633+Etchial@users.noreply.github.com>
2022-12-08 13:56:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Etchial/MdiskSearchBot
+cd /MdisksearchBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
